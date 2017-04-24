7c922ba25ffd322752c655cdb038a8e89665b93d
VijayAaric <vijaydani.smart@gmail.com>
2017-04-24 14:30:42 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+int a;
+cout<<"value of a ";
+cin>>a;
+if(a>0)
+ {
+ cout<<"\npositive";
+ }
+ else if(a<0)
+ {
+ cout<<"\nnegative";
+ }
+ else
+ {
+ cout<<"zero";
+ }
+
+ return 0;
+}
+
